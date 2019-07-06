a14f7f7a7e08dfc6611641eb9b5a262f6b485fc8
pex-1 <danijel.pecek@estudent.hr>
2019-07-06 20:06:48 +0200

    homework 1 first commit

---
+++ app/src/main/java/infinum/academy2019/shows_danijel_pecek/MainActivity.kt
@@ -0,0 +1,12 @@
+package infinum.academy2019.shows_danijel_pecek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
