156096011f1f94b88132667c0ba86efc6f120031
pex-1 <danijel.pecek@estudent.hr>
2019-07-09 20:41:07 +0200

    copy/paste zadaca 2

---
+++ app/src/main/java/infinum/academy2019/shows_danijel_pecek/WelcomeActivity.kt
@@ -0,0 +1,27 @@
+package infinum.academy2019.shows_danijel_pecek
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+
+    companion object {
+        const val USERNAME = "USERNAME"
+
+        fun newInstance(context: Context, username: String): Intent {
+            val intent = Intent(context, WelcomeActivity::class.java)
+            intent.putExtra(USERNAME, username)
+            return intent
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        welcomeTextView.text = "Welcome ${intent.getStringExtra(USERNAME)}"
+    }
+}
